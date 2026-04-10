01f9dc8add2834171e67d09c579cf4813852df03
Scirelgar <81189385+Scirelgar@users.noreply.github.com>
2026-04-10 13:28:57 -0400

    Add Coin class and initialize grid layout in CoinDisplay for coin alignment

--- src/view/visualization/coin_display.py
+++ src/view/visualization/coin_display.py
@@ -1,8 +1,22 @@
 """This module defines the CoinDisplay class for showing the current coin state in the MonarQ Heads or Tails demo."""
 
 import flet as ft
-from flet import Image, Container, Row, Column
+from flet import Image, Container, Row, Column, GridView
 from ..theme import Theme
+
+
+class Coin(Image):
+    """Custom Image class for representing a coin in the game."""
+
+    def __init__(self, src: str):
+        """
+        Initialize the Coin with a specific image source.
+
+        :param src: The file path to the coin image to display.
+        """
+        super().__init__(
+            src=src,
+        )
 
 
 class CoinDisplay(Container):
@@ -12,9 +26,31 @@ class CoinDisplay(Container):
         """
         Initialize the container, its displaying properties and the default alignment.
 
+
         :param coins: A list of Image controls representing the coins to be displayed.
         :type coins: list[Image]
         """
         super().__init__(
-            padding=20, bgcolor=Theme.ACCENT_COLOR, border_radius=10, expand=True
+            padding=20,
+            bgcolor=Theme.ACCENT_COLOR,
+            border_radius=10,
+            expand=True,
         )
+        self._initialize_grid(len(coins) if coins is not None else 0)
+
+    def _initialize_grid(self, num_elements: int = 0):
+        """
+        Initialize the grid layout displaying elements with somewhat even spacing.
+
+        Elements will be arranged in a grid with a maximum of 4 rows, and the maximum number
+        of elements it can render is 24.
+
+
+        :param num_elements: Number of elements to display.
+        :type num_elements: int
+        :return:
+        """
+
+        if num_elements == 0:
+            self.content = ft.Text("No coins to display.")
+            return
